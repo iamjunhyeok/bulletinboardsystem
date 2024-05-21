59e3888ac2289af7bf7cd7b603af678b37dd5757
iamjunhyeok <jeonjhyeok@gmail.com>
2024-05-21 20:07:42 +0900

    사용자 암호 보안을 위한 salt 추가

--- src/main/java/com/iamjunhyeok/bulletinboardsystem/mapper/UserMapper.java
+++ src/main/java/com/iamjunhyeok/bulletinboardsystem/mapper/UserMapper.java
@@ -7,9 +7,11 @@ import org.apache.ibatis.annotations.Param;
 @Mapper
 public interface UserMapper {
 
-    int join(@Param("userId") String userId, @Param("password") String password);
+    int join(@Param("userId") String userId, @Param("password") String password, @Param("salt") String salt);
 
     UserDto findByUserId(@Param("userId") String userId);
 
     UserDto findByUserIdAndPassword(@Param("userId") String userId, @Param("password") String password);
+
+    String getSaltByUserId(@Param("userId") String userId);
 }
--- src/main/java/com/iamjunhyeok/bulletinboardsystem/service/UserService.java
+++ src/main/java/com/iamjunhyeok/bulletinboardsystem/service/UserService.java
@@ -3,7 +3,6 @@ package com.iamjunhyeok.bulletinboardsystem.service;
 import com.iamjunhyeok.bulletinboardsystem.dto.UserDto;
 import com.iamjunhyeok.bulletinboardsystem.dto.request.UserJoinRequest;
 import com.iamjunhyeok.bulletinboardsystem.dto.request.UserLoginRequest;
-import com.iamjunhyeok.bulletinboardsystem.dto.response.UserLoginResponse;
 import com.iamjunhyeok.bulletinboardsystem.exception.DuplicateUserIdException;
 import com.iamjunhyeok.bulletinboardsystem.mapper.UserMapper;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +11,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import static com.iamjunhyeok.bulletinboardsystem.utils.SHA256Util.encryptSHA256;
+import static com.iamjunhyeok.bulletinboardsystem.utils.SHA256Util.getSalt;
 
 @Service
 @Transactional(readOnly = true)
@@ -27,8 +27,9 @@ public class UserService {
         if (byUserId != null) {
             throw new DuplicateUserIdException("중복된 아이디입니다.");
         }
-        request.setPassword(encryptSHA256(request.getPassword()));
-        int insertCount = userMapper.join(request.getUserId(), request.getPassword());
+        String salt = getSalt();
+        request.setPassword(encryptSHA256(request.getPassword(), salt));
+        int insertCount = userMapper.join(request.getUserId(), request.getPassword(), salt);
         if (insertCount != 1) {
             log.error("join ERROR : {}", request);
             throw new RuntimeException("회원가입에 실패했습니다.");
@@ -37,7 +38,8 @@ public class UserService {
 
     @Transactional
     public UserDto login(UserLoginRequest request) {
-        request.setPassword(encryptSHA256(request.getPassword()));
+        String salt = userMapper.getSaltByUserId(request.getUserId());
+        request.setPassword(encryptSHA256(request.getPassword(), salt));
         UserDto byUserIdAndPassword = userMapper.findByUserIdAndPassword(request.getUserId(), request.getPassword());
         return byUserIdAndPassword;
     }
